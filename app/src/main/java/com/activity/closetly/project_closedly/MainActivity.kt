d9024ce86415a0f4fcc575c885d39de7a92cd60c
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-11-11 14:52:25 -0600

    pruebita

--- app/src/main/java/com/activity/closetly/project_closedly/MainActivity.kt
+++ app/src/main/java/com/activity/closetly/project_closedly/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.activity.closetly.project_closedly.ui.theme.Project_ClosetlyTheme
 
+//prueba
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
