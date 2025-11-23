b6ab7b80853dc5f62a7dc768967cf01a8275c867
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-11-22 19:11:09 -0600

    gradle modiifficado

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,15 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file - Este archivo va en la RAÍZ del proyecto
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    // Hilt
-    id("com.google.dagger.hilt.android") version "2.57.2" apply false
-    // KSP
+
+    // KSP - necesario para Room y Hilt
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+
+    // Hilt
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+
     // Google Services (Firebase)
-    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
