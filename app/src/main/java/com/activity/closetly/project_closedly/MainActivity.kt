857482b54c896abd3c5552226258ab71f8e66155
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-11-22 18:58:58 -0600

    ctualizacio de main

--- app/src/main/java/com/activity/closetly/project_closedly/MainActivity.kt
+++ app/src/main/java/com/activity/closetly/project_closedly/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.activity.closetly.project_closedly // -> CORREGIDO
+package com.activity.closetly.project_closedly
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -7,11 +7,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-// Importaciones corregidas para apuntar al paquete 'project_closedly'
-import com.activity.closetly.project_closedly.ui.login.LoginScreen // -> CORREGIDO
-import com.activity.closetly.project_closedly.ui.theme.Project_ClosetlyTheme // -> CORREGIDO
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.activity.closetly.project_closedly.navegation.NavGraph
+import com.activity.closetly.project_closedly.navegation.Routes
+import com.activity.closetly.project_closedly.ui.theme.Project_ClosetlyTheme
+import com.activity.closetly.project_closedly.ui.viewmodel.AuthStateViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +28,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // La única responsabilidad del MainActivity es llamar a la pantalla principal.
-                    LoginScreen()
+                    val authStateViewModel: AuthStateViewModel = hiltViewModel()
+                    val isLoggedIn by authStateViewModel.isUserLoggedIn.collectAsState()
+
+                    val startDestination = if (isLoggedIn) Routes.HOME else Routes.LOGIN
+
+                    NavGraph(startDestination = startDestination)
                 }
             }
         }
