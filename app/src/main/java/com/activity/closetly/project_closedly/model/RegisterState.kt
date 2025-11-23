13a62e620a085ffe95730c9ac4932b619c9ace81
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-11-22 19:12:36 -0600

    regst¿ister state md¿¿odificado

--- app/src/main/java/com/activity/closetly/project_closedly/model/RegisterState.kt
+++ app/src/main/java/com/activity/closetly/project_closedly/model/RegisterState.kt
@@ -1,11 +1,14 @@
-package com.example.closetly.data.model
+package com.activity.closetly.project_closedly.model
 
 data class RegisterState(
     val username: String = "",
     val email: String = "",
     val password: String = "",
+    val confirmPassword: String = "",
     val acceptedTerms: Boolean = false,
     val isPasswordVisible: Boolean = false,
+    val isConfirmPasswordVisible: Boolean = false,
     val isLoading: Boolean = false,
-    val errorMessage: String? = null
+    val errorMessage: String? = null,
+    val isRegisterSuccess: Boolean = false
 )
