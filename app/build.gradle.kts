4ddb291be238d9a0b76018f27a53a84619419bde
Chafael <rafael_ruiz11@icloud.com>
2025-11-26 21:46:20 -0600

    Agregación de implements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.compose.material:material-icons-extended:1.6.0")
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth.ktx)
+    implementation(libs.firebase.firestore.ktx)
 }
