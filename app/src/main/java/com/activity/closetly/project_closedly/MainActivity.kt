0fa7543988b649d85b38c431a8a941c1fc7a53ce
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-12-08 08:04:05 -0600

    main actualizado

--- app/src/main/java/com/activity/closetly/project_closedly/MainActivity.kt
+++ app/src/main/java/com/activity/closetly/project_closedly/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
 import com.activity.closetly.project_closedly.navegation.NavGraph
 import com.activity.closetly.project_closedly.navegation.Routes
 import com.activity.closetly.project_closedly.ui.theme.Project_ClosetlyTheme
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import com.activity.closetly.project_closedly.ui.viewmodel.AuthStateViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -29,7 +31,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App() {
     val navController = rememberNavController()
-    val startDestination = if (Firebase.auth.currentUser != null) Routes.WARDROBE else Routes.LOGIN
+    val authStateViewModel: AuthStateViewModel = hiltViewModel()
+    val isUserLoggedIn by authStateViewModel.isUserLoggedIn.collectAsState()
+
+    val startDestination = if (isUserLoggedIn) Routes.WARDROBE else Routes.LOGIN
+
     NavGraph(
         navController = navController,
         startDestination = startDestination
