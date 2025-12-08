56c59fe76c4c09203cdefaa6c33d35122b448d8d
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-12-07 20:54:03 -0600

    odificacion

--- app/src/main/java/com/activity/closetly/project_closedly/ui/viewmodel/ProfileUiState.kt
+++ app/src/main/java/com/activity/closetly/project_closedly/ui/viewmodel/ProfileUiState.kt
@@ -4,13 +4,18 @@ data class ProfileUiState(
     val email: String = "",
     val username: String = "",
     val memberSince: Long = 0,
+
     val newEmail: String = "",
-    val currentPassword: String = "",
+    val emailAuthPassword: String = "",
+
+    val passwordAuthPassword: String = "",
     val newPassword: String = "",
     val confirmNewPassword: String = "",
+
     val isPasswordVisible: Boolean = false,
     val isNewPasswordVisible: Boolean = false,
     val isConfirmPasswordVisible: Boolean = false,
+
     val isUpdatingEmail: Boolean = false,
     val isUpdatingPassword: Boolean = false,
     val successMessage: String? = null,
