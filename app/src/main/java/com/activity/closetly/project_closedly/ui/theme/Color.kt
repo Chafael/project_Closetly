c0c056729b78109b1107ec43360b061e554f402a
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-12-07 23:09:20 -0600

    colores

--- app/src/main/java/com/activity/closetly/project_closedly/ui/theme/Color.kt
+++ app/src/main/java/com/activity/closetly/project_closedly/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val brown = Color(0xFF6D5D52)
+val lightbrown = Color(0xFFB59A7A)
