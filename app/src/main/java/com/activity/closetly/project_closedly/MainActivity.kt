189caa0ff9d862a39142cb595a1bb17fadb8f6f7
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-12-07 21:31:36 -0600

    Main modificado

--- app/src/main/java/com/activity/closetly/project_closedly/MainActivity.kt
+++ app/src/main/java/com/activity/closetly/project_closedly/MainActivity.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.activity.closetly.project_closedly.navegation.NavGraph
-import com.activity.closetly.project_closedly.navegation.Routes
+import androidx.activity.viewModels
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import com.activity.closetly.project_closedly.navigation.NavGraph
 import com.activity.closetly.project_closedly.ui.theme.Project_ClosetlyTheme
 import com.activity.closetly.project_closedly.ui.viewmodel.AuthStateViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,25 +15,24 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    private val authViewModel: AuthStateViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         enableEdgeToEdge()
-
         setContent {
             Project_ClosetlyTheme {
-                val authViewModel: AuthStateViewModel = hiltViewModel()
-
-                val isUserLoggedIn by authViewModel.isUserLoggedIn.collectAsState()
-
-                val startDestination = if (isUserLoggedIn) {
-                    Routes.WARDROBE
-                } else {
-                    Routes.LOGIN
-                }
-
-                NavGraph(startDestination = startDestination)
+                App(authViewModel)
             }
         }
     }
+}
+
+@Composable
+fun App(authViewModel: AuthStateViewModel) {
+    val navController = rememberNavController()
+    NavGraph(
+        navController = navController,
+        authViewModel = authViewModel
+    )
 }
