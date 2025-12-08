477b260d07fbb358e20c41b1b76926672c26edd3
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-12-07 21:39:51 -0600

    main modificado

--- app/src/main/java/com/activity/closetly/project_closedly/MainActivity.kt
+++ app/src/main/java/com/activity/closetly/project_closedly/MainActivity.kt
@@ -4,35 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
 import com.activity.closetly.project_closedly.navigation.NavGraph
 import com.activity.closetly.project_closedly.ui.theme.Project_ClosetlyTheme
-import com.activity.closetly.project_closedly.ui.viewmodel.AuthStateViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    private val authViewModel: AuthStateViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Project_ClosetlyTheme {
-                App(authViewModel)
+                App()
             }
         }
     }
 }
 
 @Composable
-fun App(authViewModel: AuthStateViewModel) {
+fun App() {
     val navController = rememberNavController()
-    NavGraph(
-        navController = navController,
-        authViewModel = authViewModel
-    )
+    NavGraph(navController = navController)
 }
